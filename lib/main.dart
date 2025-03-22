6a151ce1f22fd36aada16f89b01be9acb5763fd6
duevanofairuz <duevanofairuz@gmail.com>
2025-03-22 13:21:44 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Halo dunia!"),
+    );
+  }
+}
+
